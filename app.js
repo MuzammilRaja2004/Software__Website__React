2dd9ded08db11d431d558c81d10909cc187f553e
syedalihussain100 <smuheminali@gmail.com>
2021-10-01 05:44:48 -0700

    changes

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
-const data = (name) => {
-    return `Hello ${name}`
-}
-
-const check1 = data('Muhemin');
-console.log(check1)
-=======
-alert("Hello World")
->>>>>>> f03b8b811b5cd4d345d389a50b2d4d2ee3dfebcf
+let array = ['hammad','farhan','zaid'];
+console.log(array)
