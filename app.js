a0ec75980ed95dd55a30f85c0ced86b1b4616998
syedalihussain100 <smuheminali@gmail.com>
2021-10-01 05:41:03 -0700

    fixed problem

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 const data = (name) => {
     return `Hello ${name}`
 }
 
 const check1 = data('Muhemin');
 console.log(check1)
+=======
+alert("Hello World")
+>>>>>>> f03b8b811b5cd4d345d389a50b2d4d2ee3dfebcf
